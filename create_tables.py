5bcf9ba2407865cec815dfb6953a4872056d8073
David Li <davidli012345@gmail.com>
2022-07-06 04:04:30 +0000

    feat: create basic table using cockroach db

---
+++ create_tables.py
@@ -0,0 +1,15 @@
+import os
+import psycopg
+
+conn_dict =  psycopg.conninfo.conninfo_to_dict(os.environ["COCKROACH_DB_URL"])
+
+print(conn_dict)
+with psycopg.connect(**conn_dict) as conn:
+    conn.execute("DROP TABLE IF EXISTS events")
+    # create simple table named events with date,title,description,source
+    #   source?: string
+    conn.execute("CREATE TABLE events (date date, title text, description text, source text, country text, exchange text, url text, company text)")
+    conn.commit()
+    # insert some data
+    conn.execute("INSERT INTO events VALUES ('2020-01-01', 'New Year', 'Happy New Year', 'source', 'country', 'exchange', 'url', 'company')")
+    print("Table events created")
---
+++ read_table.py
@@ -0,0 +1,9 @@
+import os
+import psycopg
+
+conn_dict =  psycopg.conninfo.conninfo_to_dict(os.environ["DATABASE_URL"])
+
+print(conn_dict)
+with psycopg.connect(**conn_dict) as conn:
+    cursor = conn.execute("SELECT * FROM events");
+    print(cursor.fetchall())
