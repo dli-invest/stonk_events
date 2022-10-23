73c6da644324b96a418cfaff1970d234a62318e8
David Li <davidli012345@gmail.com>
2022-10-23 21:50:32 +0000

    feat: fixing postmarket and premarket

--- src/send_discord.py
+++ src/send_discord.py
@@ -1,18 +1,24 @@
 import os 
 import requests
+import argparse
 
 
-def send_file(path: str) -> None:
+def send_file(path: str, content = "MarketWatch Report", username="Marketwatch") -> None:
     """ send discord file from path to discord using requests"""
     url = os.getenv("DISCORD_WEBHOOK")
     files = {'file': open(path, 'rb')}
     data = {
-        "username": "MarketWatch",
-        "content": "MarketWatch Report",
+        "username": username,
+        "content": content,
     }
     requests.post(url, files=files, data=data)
 
     
 
 if __name__ == '__main__':
-    send_file("latest.pdf")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, required=False, default="latest.pdf")
+    parser.add_argument("--content", type=str, required=False, default="MarketWatch Report")
+    parser.add_argument("--username", type=str, required=False, default="Marketwatch")
+    args = parser.parse_args()
+    send_file(args.path, args.content, args.username)
